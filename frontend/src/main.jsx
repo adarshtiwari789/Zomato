2a32e18854869c9ae64fc1032290a94b7cd114a2
adarshtiwari789 <aadarshh1789@gmail.com>
2025-12-13 13:00:14 +0530

    refix of main.jsx to use hashrotuer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>          {/* ← Add this */}
+    <HashRouter>
       <App />
-    </BrowserRouter>         {/* ← And this */}
+    </HashRouter>
   </React.StrictMode>
-  )
+)
